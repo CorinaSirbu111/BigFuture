2b165611bbed745944142587eebe442fdc71575d
CorinaSirbu111 <nastasa_corina@yahoo.com>
2020-11-03 10:55:09 +0000

    Task completed

--- src/test/java/com/libraryCT/runners/CukesRunner.java
+++ src/test/java/com/libraryCT/runners/CukesRunner.java
@@ -1,9 +1,22 @@
 package com.libraryCT.runners;
 
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports",
+                "rerun:target/rerun.txt"},
+        features = "src/test/resources/features",
+        glue = "com/libraryCT/step_definitions",
+
+
+        dryRun = false,
+        tags = "@wip"
+)
 public class CukesRunner {
-
-    public static void main(String[] args) {
-        System.out.println("");
-    }
-
 }
---
+++ src/test/java/com/libraryCT/step_definitions/Login_stepdef.java
@@ -0,0 +1,43 @@
+package com.libraryCT.step_definitions ;
+
+
+import com.libraryCT.pages.LoginPage;
+import com.libraryCT.utilities.BrowserUtils;
+import com.libraryCT.utilities.ConfigurationReader;
+import com.libraryCT.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+
+public class Login_stepdef {
+
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
+        WebDriver driver = Driver.get();
+        String url = ConfigurationReader.get("url");
+        driver.get(url);
+
+
+    }
+
+    @When("the user logs in using {string} and {string}")
+    public void the_user_logs_in_using_and(String username, String password) {
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(username, password);
+
+    }
+
+    @Then("the user should be able to log in and see the dashboard page")
+    public void the_user_should_be_able_to_log_in_and_see_the_dashboard_page() {
+        BrowserUtils.waitFor(3);
+        String actualTitle = Driver.get().getTitle();
+        Assert.assertEquals("Library",actualTitle);
+
+    }
+
+
+
+
+}
